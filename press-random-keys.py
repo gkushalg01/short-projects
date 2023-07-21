aae1c5b86aabff1f92307d43af8a80dd1658e9d4
kushal <gkushalg01@gmail.com>
2023-07-21 17:12:04 +0530

    10/06/2023 - key pressing program for afk-ing

---
+++ press-random-keys.py
@@ -0,0 +1,59 @@
+"""
+A small program to not get banned when you are AFK for 2-3 rounds on valorant 
+or any other game that instantly bans you for going AFK. AFKaying is distasteful
+but sometimes necessary. So run this program and press 9 to start pressing 
+random keys on your keyboard.
+Hold 8 to stop the program altogether. 
+"""
+
+import pyautogui
+import time
+import random
+import keyboard
+
+def random_key_press():
+    keys = ['up', 'down', 'right', 'left']
+    key = random.choice(keys)
+    pyautogui.keyDown(key)
+    time.sleep(0.1)  # Adjust the delay as per your preference
+    pyautogui.keyUp(key)
+
+def key_press_listener():
+    run = True
+    while run:
+        if keyboard.is_pressed('9'):
+            while True:
+                time.sleep(1)
+                random_key_press()
+                if keyboard.is_pressed('8'):
+                    run = False
+                    break  # Exit the loop when '8' key is pressed
+
+# Start the key press listener
+if __name__=="__main__":
+    print("Press '9' to start and Hold '8' to stop.\n")
+    key_press_listener()
+
+
+all_keys_that_pyautogui_supports = ['\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
+')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
+'8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
+'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
+'browserback', 'browserfavorites', 'browserforward', 'browserhome',
+'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
+'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
+'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
+'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
+'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
+'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
+'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
+'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
+'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
+'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
+'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
+'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
+'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab',
+'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen',
+'command', 'option', 'optionleft', 'optionright']
